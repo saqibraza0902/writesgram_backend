51b2ad031ccbdd975f46f6e1b0e7028e506d1820
unknown <saqibraza0902@gmail.com>
2023-09-25 12:11:56 +0500

    fixing

--- src/routes/Blog.routes.ts
+++ src/routes/Blog.routes.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import { AddBlog, GetBlog } from '../controllers/Blog.controller';
-import { GetSingleBlog } from '../controllers/Blog.controller';
+import express from 'express'
+import { AddBlog, GetBlog } from '../controllers/Blog.controller'
+import { GetSingleBlog, GetFeaturedPosts } from '../controllers/Blog.controller'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/add-blog', AddBlog);
-router.get('/get-blog', GetBlog);
-router.get('/get-single-blog', GetSingleBlog);
-
-export default router;
+router.post('/add-blog', AddBlog)
+router.get('/get-blog', GetBlog)
+router.get('/get-single-blog', GetSingleBlog)
+router.get('/featured', GetFeaturedPosts)
+export default router
