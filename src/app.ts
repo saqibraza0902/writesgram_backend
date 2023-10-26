03c2afaebcfc8b14c89f67238bf5523fba5ac800
unknown <saqibraza0902@gmail.com>
2023-10-26 18:45:51 +0500

    fixing deployment

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,31 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import BlogRoutes from './routes/Blog.routes';
-import UserRoutes from './routes/Users.routes';
-import bodyParser from 'body-parser';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import mongoose from "mongoose";
+import BlogRoutes from "./routes/Blog.routes";
+import UserRoutes from "./routes/Users.routes";
+import bodyParser from "body-parser";
 
-require('dotenv').config();
+require("dotenv").config();
 const mongoURI = process.env.MONGO_URI as string;
 mongoose
   .connect(mongoURI)
   .then(async () => {
     try {
     } catch (error) {}
-    console.log('DB Connected.');
+    console.log("DB Connected.");
   })
   .catch((err) => console.log(err));
 const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(morgan('dev'));
+app.use(express.json({ limit: "50mb" }));
+app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-
-app.use('/blog', BlogRoutes);
-app.use('/user', UserRoutes);
+app.get("/", (req, res) => {
+  res.send("HEllo");
+});
+app.use("/blog", BlogRoutes);
+app.use("/user", UserRoutes);
 export default app;
